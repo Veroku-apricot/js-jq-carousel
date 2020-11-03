fcbfbfce537b72783a34e75baf5434b30e00866c
Veronika <veronikakuchtova95@gmail.com>
2020-11-03 16:13:44 +0100

    Made the circles change color too

--- js/script.js
+++ js/script.js
@@ -1,49 +1,81 @@
 $(document).ready(function () {
 
-  $(".next").click(nextImg);
-  $(".prev").click(prevImg);
+  $(".next").click(nextImg).click(nextCircle);
+  $(".prev").click(prevImg).click(prevCircle);
 
   // Browse img using keyboard arrows
   $(document).keydown(function(e){
    var keypressed = e.which;
    if (keypressed == 39 || keypressed == 68){
      nextImg();
+     nextCircle();
      console.log('pressed next');
    }else if(keypressed == 37 || keypressed == 65){
      prevImg();
+     prevCircle();
      console.log('pressed prev');
    }
   });
 
 
   //FUNCTIONS
-  // function next
+  // function next img
   function nextImg() {
-    var imgAttiva = $("img.active");
-    imgAttiva.removeClass("active");
+    var imgActive = $("img.active");
+    imgActive.removeClass("active");
     // If last img
-    if (imgAttiva.hasClass("last")){
+    if (imgActive.hasClass("last")){
       //Go back to first img
       $("img.first").addClass("active");
     }
-    // If not, pass .attiva to the next img
+    // If not, pass .active to the next img
     else {
-      imgAttiva.next("img").addClass("active");
+      imgActive.next("img").addClass("active");
     }
   }
 
-  // function previous
+  // function previous img
   function prevImg() {
-    var imgAttiva = $("img.active");
-    imgAttiva.removeClass("active");
+    var imgActive = $("img.active");
+    imgActive.removeClass("active");
     // If first img
-    if (imgAttiva.hasClass("first")){
+    if (imgActive.hasClass("first")){
       //Go to last img
       $("img.last").addClass("active");
     }
-    // If not, pass .attiva to the previous img
+    // If not, pass .active to the previous img
     else {
-      imgAttiva.prev("img").addClass("active");
+      imgActive.prev("img").addClass("active");
+    }
+  }
+
+  //function circle next
+  function nextCircle() {
+    var activeCircle = $("i.active");
+    activeCircle.removeClass("active");
+    // If last circle
+    if (activeCircle.hasClass("last")){
+      //Go back to first circle
+      $("i.first").addClass("active");
+    }
+    // If not, pass .active to the next circle
+    else {
+      activeCircle.next("i").addClass("active");
+    }
+  }
+
+  //function circle prev
+  function prevCircle() {
+    var activeCircle = $("i.active");
+    activeCircle.removeClass("active");
+    // If first circle
+    if (activeCircle.hasClass("first")){
+      //Go to last circle
+      $("i.last").addClass("active");
+    }
+    // If not, pass .active to the prev circle
+    else {
+      activeCircle.prev("i").addClass("active");
     }
   }
 });
